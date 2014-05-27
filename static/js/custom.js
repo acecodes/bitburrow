8454a67a98d65427677d7c1e1dfe61ef34fdaa07
Ace <ace.github@gmail.com>
2014-05-26 22:18:18 -0700

    Removed unwanted JS file

--- static/js/custom.js
+++
@@ -1,9 +0,0 @@
-$( "#encode" ).submit(function( event ) {
-  var value = {{ encode('Boom')|tojson|safe }};
-  $( "h3.results" ).text( value );
-  event.preventDefault();
-});
-
-$( "#results" ).click(function() {
-  $( "#encode" ).submit();
-});
