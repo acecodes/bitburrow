20f26ca41abde2f6f8d51d9f71892d5d7ae40e8b
Ace <ace.github@gmail.com>
2014-06-30 18:25:16 -0700

    Refined functional test to include testing inputs forms on front page

--- functional_test.py
+++ functional_test.py
@@ -1,16 +1,39 @@
 # Runs a functional test on BitBurrow to ensure it is working
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 import unittest
+
+test_english = 'Hello world!'
+test_morse = ".... . .-.. .-.. --- .-- --- .-. .-.. -.. -.-.--"
+
+local_test = True
+
+if local_test == True:
+	url = 'http://localhost:5000'
+else:
+	url = 'http://www.bitburrow.com'
+
 
 class BitBurrowTester(unittest.TestCase):
 	# Create browser driver and open the page
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-		self.browser.get('http://www.bitburrow.com')
-
-	# Makes sure that the page is not returning an error or anything
-	def test_online(self):
+		self.browser.get(url)
 		self.assertIn('BitBurrow', self.browser.title)
+		
+	# Run a test of the encoding function
+	def test_encoder(self):
+		encoder_input = self.browser.find_element_by_name('encoder_message')
+		encoder_input.send_keys(test_english)
+		encoder_input.send_keys(Keys.RETURN)
+		sleep(5)
+
+	def test_decoder(self):
+		decoder_input = self.browser.find_element_by_name('decoder_message')
+		decoder_input.send_keys(test_morse)
+		decoder_input.send_keys(Keys.RETURN)
+		sleep(5)
 
 	# Close the browser window
 	def tearDown(self):
