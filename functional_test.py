0e1963c749d23bdb617507bab8e7691b40fc5c66
Ace <ace.github@gmail.com>
2015-07-02 18:03:40 -0700

    Reformatted test script for PEP 8

--- functional_test.py
+++ functional_test.py
@@ -10,34 +10,36 @@ test_morse = ".... . .-.. .-.. --- .--.-. .-- --- .-. .-.. -.. -.-.--"
 local_test = True
 
 if local_test == True:
-	url = 'http://localhost:5000'
+    url = 'http://localhost:5000'
 else:
-	url = 'http://www.bitburrow.com'
+    url = 'http://www.bitburrow.com'
 
 
 class BitBurrowTester(unittest.TestCase):
-	# Create browser driver and open the page
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.get(url)
-		self.assertIn('BitBurrow', self.browser.title)
-		
-	# Run a test of the encoding function
-	def test_encoder(self):
-		encoder_input = self.browser.find_element_by_name('encoder_message')
-		encoder_input.send_keys(test_english)
-		encoder_input.send_keys(Keys.RETURN)
-		sleep(5)
-	# Run a test of the decoding function
-	def test_decoder(self):
-		decoder_input = self.browser.find_element_by_name('decoder_message')
-		decoder_input.send_keys(test_morse)
-		decoder_input.send_keys(Keys.RETURN)
-		sleep(5)
+    # Create browser driver and open the page
 
-	# Close the browser window
-	def tearDown(self):
-		self.browser.close()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.get(url)
+        self.assertIn('BitBurrow', self.browser.title)
+
+    # Run a test of the encoding function
+    def test_encoder(self):
+        encoder_input = self.browser.find_element_by_name('encoder_message')
+        encoder_input.send_keys(test_english)
+        encoder_input.send_keys(Keys.RETURN)
+        sleep(5)
+    # Run a test of the decoding function
+
+    def test_decoder(self):
+        decoder_input = self.browser.find_element_by_name('decoder_message')
+        decoder_input.send_keys(test_morse)
+        decoder_input.send_keys(Keys.RETURN)
+        sleep(5)
+
+    # Close the browser window
+    def tearDown(self):
+        self.browser.close()
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
